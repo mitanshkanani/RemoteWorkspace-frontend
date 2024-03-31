a581ac70dfb8968316362c4abb062f2b97f79989
mitanshkanani <143603367+mitanshkanani@users.noreply.github.com>
2024-03-31 23:17:11 +0530

    added the content of the rootlayout page and Hompage  and connected it

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,15 +1,93 @@
-import React from 'react'
-import { Outlet, Link } from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import { styled } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+import { Box } from "@mui/material";
+
+const BootstrapButton = styled(Button)({
+  boxShadow: "none",
+  textTransform: "none",
+  fontSize: 16,
+  padding: "10px 20px", 
+  border: "1px solid",
+  lineHeight: 1.5,
+  backgroundColor: "#0063cc",
+  borderColor: "#0063cc",
+  borderRadius: "20px",
+  "&:hover": {
+    backgroundColor: "#0069d9",
+    borderColor: "#0062cc",
+    boxShadow: "none",
+  },
+  "&:active": {
+    boxShadow: "none",
+    backgroundColor: "#0062cc",
+    borderColor: "#005cbf",
+  },
+  "&:focus": {
+    boxShadow: "0 0 0 0.2rem rgba(0,123,255,.5)",
+  },
+});
 
 const RootLayout = () => {
-    return (
-        <div>
-            <Link to="login" >Login</Link>
-            <Link to="register" >Register</Link>
-            <Link to="workspace">Workspace</Link>
-            <Outlet />
+  return (
+    <Box
+      style={{
+        background: "#141414",
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        position: "absolute",
+        top: 0,
+        left: 0,
+      }}
+    >
+      <Box style={{ border: "1px solid white", borderRadius: "30px", width: "fit-content", padding: "20px" }}>
+        <div
+          style={{
+            fontFamily: "cursive",
+            fontSize: "60px",
+            color: "purple",
+            width: "400px",
+            textAlign: "center",
+          }}
+        >
+          <Box mb={1}>
+            <h4 style={{ color: "white", marginBottom: "5px" }}>Come In !</h4>
+            <hr style={{ borderTop: "2px solid white", width: "200px", margin: "0 auto 10px auto" }} />
+            <p style={{ color: "white", fontSize: "20px" }}>
+              Please Select one of the below to continue
+            </p>
+            <Box mb={1}>
+              <BootstrapButton
+                variant="contained"
+                style={{ background: "#3C1361", width: "169px", height: "44px", marginBottom: "10px" }}
+              >
+                <Link
+                  to="/login"
+                  style={{ textDecoration: "none", color: "white" }}
+                >
+                  Log In
+                </Link>
+              </BootstrapButton>
+            </Box>
+            <BootstrapButton variant="contained" style={{ background: "white", width: "169px", height: "44px" }}>
+              <Link
+                to="/register"
+                style={{ textDecoration: "none", color: "#7C5294" }}
+              >
+                Sign Up
+              </Link>
+            </BootstrapButton>
+          </Box>
+          <Outlet />
         </div>
-    )
-}
+      </Box>
+    </Box>
+  );
+};
 
-export default RootLayout
+export default RootLayout;
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,93 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import { styled } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+import { Box } from "@mui/material";
+
+const BootstrapButton = styled(Button)({
+  boxShadow: "none",
+  textTransform: "none",
+  fontSize: 16,
+  padding: "10px 20px", 
+  border: "1px solid",
+  lineHeight: 1.5,
+  backgroundColor: "#0063cc",
+  borderColor: "#0063cc",
+  borderRadius: "20px",
+  "&:hover": {
+    backgroundColor: "#0069d9",
+    borderColor: "#0062cc",
+    boxShadow: "none",
+  },
+  "&:active": {
+    boxShadow: "none",
+    backgroundColor: "#0062cc",
+    borderColor: "#005cbf",
+  },
+  "&:focus": {
+    boxShadow: "0 0 0 0.2rem rgba(0,123,255,.5)",
+  },
+});
 
 const Homepage = () => {
-    return (
-        <div>
-            Homepage
+  return (
+    <Box
+      style={{
+        background: "#141414",
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        position: "absolute",
+        top: 0,
+        left: 0,
+      }}
+    >
+      <Box style={{ border: "1px solid white", borderRadius: "30px", width: "fit-content", padding: "20px" }}>
+        <div
+          style={{
+            fontFamily: "cursive",
+            fontSize: "60px",
+            color: "purple",
+            width: "400px",
+            textAlign: "center",
+          }}
+        >
+          <Box mb={1}>
+            <h4 style={{ color: "white", marginBottom: "5px" }}>Welcome !</h4>
+            <hr style={{ borderTop: "2px solid white", width: "200px", margin: "0 auto 10px auto" }} />
+            <p style={{ color: "white", fontSize: "20px" }}>
+            Select the mode you would like to work in, next.
+            </p>
+            <Box mb={1}>
+              <BootstrapButton
+                variant="contained"
+                style={{ background: "#3C1361", width: "169px", height: "44px", marginBottom: "10px" }}
+              >
+                <Link
+                  to="/login"
+                  style={{ textDecoration: "none", color: "white" }}
+                >
+                 Personal 
+                </Link>
+              </BootstrapButton>
+            </Box>
+            <BootstrapButton variant="contained" style={{ background: "white", width: "169px", height: "44px" }}>
+              <Link
+                to="/register"
+                style={{ textDecoration: "none", color: "#7C5294" }}
+              >
+                Collaborative
+              </Link>
+            </BootstrapButton>
+          </Box>
+          <Outlet />
         </div>
-    )
-}
+      </Box>
+    </Box>
+  );
+};
 
-export default Homepage
+export default Homepage;
